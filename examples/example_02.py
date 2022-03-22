49b9860dc5defd712c3a64274625a04cc98722b2
Junfeng Qiao <qiaojunfeng@outlook.com>
2022-03-22 19:42:44 +0100

    Update CLI

--- examples/example_02.py
+++ examples/example_02.py
@@ -7,7 +7,7 @@ import click
 
 from aiida import cmdline, orm
 
-from aiida_wannier90_workflows.cli.types import FilteredWorkflowParamType
+from aiida_wannier90_workflows.cli.params import RUN, FilteredWorkflowParamType
 from aiida_wannier90_workflows.utils.workflows.builder import (
     print_builder,
     submit_and_add_group,
@@ -19,7 +19,9 @@ from aiida_wannier90_workflows.workflows import Wannier90BandsWorkChain
 
 
 def submit(
-    w90_wkchain: Wannier90BandsWorkChain, group: orm.Group = None, dry_run: bool = True
+    w90_wkchain: Wannier90BandsWorkChain,
+    group: orm.Group = None,
+    run: bool = False,
 ):
     """Submit a ``PwBaseWorkChain`` to calculate PW bands.
 
@@ -35,7 +37,7 @@ def submit(
 
     print_builder(builder)
 
-    if not dry_run:
+    if run:
         submit_and_add_group(builder, group)
 
 
@@ -49,24 +51,17 @@ def submit(
         )
     ),
 )
-@click.option(
-    "--run",
-    "-r",
-    is_flag=True,
-    help="Submit workchain.",
-)
 @cmdline.params.options.GROUP(
     help="The group to add the submitted workchain.",
 )
-def cli(workflow, run, group):
+@RUN()
+def cli(workflow, group, run):
     """Run a ``PwBaseWorkChain`` for PW band structure.
 
     Reuse the scf calculation from a finished Wannier90BandsWorkChain.
     """
-    dry_run = not run
-
     # workflow = orm.load_node(139623)
-    submit(workflow, group, dry_run)
+    submit(workflow, group, run)
 
 
 if __name__ == "__main__":
