0074c656cb5f0a434445855e524b77b6608dad0d
Junfeng Qiao <qiaojunfeng@outlook.com>
2022-03-28 12:35:12 +0200

    Add option to save figure

--- aiida_yambo_wannier90/cli/plot.py
+++ aiida_yambo_wannier90/cli/plot.py
@@ -31,7 +31,10 @@ def cmd_plot():
 @click.option(
     "--w90",
     type=FilteredWorkflowParamType(
-        process_classes=("aiida.workflows:wannier90_workflows.base.wannier90",)
+        process_classes=(
+            "aiida.workflows:wannier90_workflows.base.wannier90",
+            "aiida.workflows:wannier90_workflows.bands",
+        )
     ),
     help=(
         "If provided, use this BandsData instead of Wannier90 bands from W90_QP, "
@@ -47,15 +50,29 @@ def cmd_plot():
         )
     ),
 )
+@click.option(
+    "-s",
+    "--save",
+    is_flag=True,
+    default=False,
+    help="Save figure instead of showing matplotlib window",
+)
+@click.option(
+    "-f",
+    "--filename",
+    type=str,
+    default=None,
+    help="Filename of the saved figure, default format is PNG.",
+)
 @decorators.with_dbenv()
-def cmd_plot_bands(pw, w90, w90_qp):
+def cmd_plot_bands(pw, w90, w90_qp, save, filename):
     """Compare PW, Wannier90, and Wannier90 QP-corrected band structures.
 
     \b
     PW: PwBandsWorkChain/PwBaseWorkChain/BandsData
     W90_QP: YamboWannier90WorkChain/Wannier90BandsWorkChain/Wannier90BaseWorkChain/BandsData
     """
-    # pylint: disable=import-outside-toplevel
+    # pylint: disable=import-outside-toplevel,too-many-locals,too-many-branches
     from aiida_wannier90_workflows.utils.workflows.plot import (
         get_band_dict,
         get_workchain_fermi_energy,
@@ -87,8 +104,12 @@ def cmd_plot_bands(pw, w90, w90_qp):
     if isinstance(pw, orm.WorkChainNode):
         fermi_energy = get_workchain_fermi_energy(pw)
     if fermi_energy is None:
-        if isinstance(w90, orm.WorkChainNode):
-            fermi_energy = get_workchain_fermi_energy(w90)
+        if isinstance(w90_qp, orm.WorkChainNode):
+            if w90_qp.process_class == YamboWannier90WorkChain:
+                calc = w90_qp.get_outgoing(link_label_filter="wannier90_qp").one().node
+                fermi_energy = get_workchain_fermi_energy(calc)
+            else:  # e.g. Wannier90BaseWorkChain
+                fermi_energy = get_workchain_fermi_energy(w90_qp)
 
     print(f"{fermi_energy = }")
 
@@ -114,4 +135,14 @@ def cmd_plot_bands(pw, w90, w90_qp):
     ax.set_title(title)
 
     plt.autoscale(axis="y")
-    plt.show()
+
+    if save:
+        if filename is None:
+            if w90 is None:
+                filename = f"bandsdiff-PW_{pw.pk}-GWW90_{w90_qp.pk}.png"
+            else:
+                filename = f"bandsdiff-PW_{pw.pk}-W90_{w90.pk}-GWW90_{w90_qp.pk}.png"
+        plt.savefig(filename)
+        print(f"Saved to {filename}")
+    else:
+        plt.show()
